91322f2e885eafc7ec55c9a0f81db66065d7593e
Anirudh Moholkar <143670965+Anirudh-M1@users.noreply.github.com>
2026-09-07 21:18:44 -0500

    Add: reverse-bits - submission-6

---
+++ Data Structures & Algorithms/reverse-bits/submission-6.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        ans = []
+        for i in range(32): 
+            n, r = divmod(n, 2)
+            ans.append(r)
+
+        a = 0
+        for i in range(31,-1, -1):
+            if ans[32-i-1]:
+                a+= 2**i
+        
+        return a
